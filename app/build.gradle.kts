e7666f973d26a9d593699e85b075aa3e7387d402
Jose Lopez Lopez <jose04lopez@gmail.com>
2024-05-02 19:50:06 +0200

    Cambios para habilitar notificaciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("androidx.core:core-ktx:1.7.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/principal/trabajofindegrado/SettingsActivity.java
+++ app/src/main/java/com/principal/trabajofindegrado/SettingsActivity.java
@@ -2,12 +2,9 @@ package com.principal.trabajofindegrado;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -18,68 +15,33 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         Switch switchNotifications = findViewById(R.id.switch_notifications);
-
-        // Inicializa el RelativeLayout correspondiente al botón de ayuda
         RelativeLayout rlHelp = findViewById(R.id.rl_help);
-
-        // Configurar eventos para las imágenes del menú inferior
         ImageButton btnToday = findViewById(R.id.btnToday);
         ImageButton btnAdd = findViewById(R.id.btnAdd);
         ImageButton btnStatistics = findViewById(R.id.btnStatistics);
 
-        // Configurar el evento de clic para el botón de ayuda
-        rlHelp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Abrir la nueva pantalla (Activity de ayuda)
-                Intent intent = new Intent(SettingsActivity.this, HelpActivity.class);
-                startActivity(intent);
-            }
+
+
+        rlHelp.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, HelpActivity.class);
+            startActivity(intent);
         });
 
-        // Configurar eventos de clic para los botones del menú inferior
-        btnToday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Iniciar MainActivity
-                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                startActivity(intent);
-                // Finalizar SettingsActivity si deseas que se cierre al abrir MainActivity
-                finish();
-            }
+        btnToday.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Agregar aquí la lógica para el botón de "Agregar"
-            }
-        });
+        switchNotifications.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
 
-        btnStatistics.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Agregar aquí la lógica para el botón de "Estadísticas"
-            }
-        });
+            } else {
 
-        // Configurar el listener para el Switch de notificaciones
-        switchNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Aquí puedes agregar la lógica para manejar el cambio de estado del Switch
-                if (isChecked) {
-                    // Si el Switch está activado (isChecked == true), puedes realizar alguna acción
-                    // Por ejemplo, mostrar un mensaje o activar las notificaciones
-                } else {
-                    // Si el Switch está desactivado (isChecked == false), puedes realizar alguna otra acción
-                    // Por ejemplo, ocultar un elemento o desactivar las notificaciones
-                }
             }
         });
     }
 
-    // Definir HelpActivity como una clase interna de SettingsActivity
     public static class HelpActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -90,31 +52,13 @@ public class SettingsActivity extends AppCompatActivity {
             ImageButton btnAdd = findViewById(R.id.btnAdd);
             ImageButton btnStatistics = findViewById(R.id.btnStatistics);
 
-            // Configurar eventos de clic para los botones del menú inferior
-            btnToday.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Iniciar MainActivity
-                    Intent intent = new Intent(HelpActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    // Finalizar SettingsActivity si deseas que se cierre al abrir MainActivity
-                    finish();
-                }
-            });
-
-            btnAdd.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Agregar aquí la lógica para el botón de "Agregar"
-                }
-            });
-
-            btnStatistics.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Agregar aquí la lógica para el botón de "Estadísticas"
-                }
+            btnToday.setOnClickListener(v -> {
+                Intent intent = new Intent(HelpActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             });
         }
     }
+
+
 }
